11daaa98b13c7fd861bdc9bb3df52e1bfe1ae1ca
Max2 <chigov@gmail.com>
2021-10-28 21:28:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_basic/string_basic/CreateMainFun.kt
@@ -0,0 +1,32 @@
+package com.example.kotlin_basic.string_basic
+
+import java.util.*
+
+fun res()
+{
+    println("hello")
+    val result = modifyString("hello2") { it.uppercase(Locale.getDefault()) }
+    println(result)
+
+}
+//функция будет принмать строку и возвращать модифицированную строку
+// modify будет принимать строку (modify:(String)) и возвращать модифицированную строку  ->String
+                                //: - тип
+fun modifyString(str: String, modify:(String)-> String): String{
+    return modify(str)
+
+}
+
+fun main ()
+{
+    val list = (0..20).toList()
+    listModify(list) {
+        println(it.sum())
+    }
+
+}
+
+inline fun listModify(list :List<Int>, operator:(List<Int>)-> Unit)
+{
+    operator(list)
+}
